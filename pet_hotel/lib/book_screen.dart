b97b4f8b5d0da4b07ae373639c5ea747b4e2ea23
Ayse Almaci <ayseeealmaciii@gmail.com>
2023-06-01 21:02:39 +0300

    edit calendar

--- pet_hotel/lib/book_screen.dart
+++ pet_hotel/lib/book_screen.dart
@@ -10,18 +10,39 @@ class BookPage extends StatefulWidget {
 
 class _BookPageState extends State<BookPage> {
   DateTime today = DateTime.now();
+
+  void _selectedDay(DateTime day, DateTime focusedDay) {
+    setState(() {
+      today = day;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Column(
         children: [
-          Text("ABC"),
+          Text("Choose The Date"),
           Container(
             child: TableCalendar(
-                focusedDay: today,
-                firstDay: DateTime.utc(2023, 6, 1),
-                lastDay: DateTime.utc(2023, 12, 31)),
-          )
+              locale: "en_US",
+              rowHeight: 50,
+              headerStyle: HeaderStyle(
+                formatButtonVisible: false,
+                titleCentered: true,
+              ),
+              availableGestures: AvailableGestures.all,
+              selectedDayPredicate: (day) => isSameDay(day, today),
+              focusedDay: today,
+              firstDay: DateTime.utc(2023, 6, 1),
+              lastDay: DateTime.utc(2023, 12, 31),
+              onDaySelected: _selectedDay,
+            ),
+          ),
+          SizedBox(
+            height: 30,
+          ),
+          Text("Selected day for booking is: " + today.toString().split(".")[0])
         ],
       ),
     );
